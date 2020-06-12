b449a43669a916130a6665cbd7702c62c1f502dc
liuchang <liuchang@126.com>
2020-06-12 23:56:19 +0800

    test

---
+++ src/test/java/com/iqiyi/ideatest/IdeatestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iqiyi.ideatest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IdeatestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
